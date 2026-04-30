53c40f9fc6c3c780861da4b36d49cf4d7571137d
Prince kumar ojha <princekumarojha@princes-MacBook-Air.local>
2026-04-30 15:57:09 +0530

    chore: Add TypeScript ambient declarations to fix IDE CDN import warnings

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,12 @@
-interface Window {
-  dataLayer: any[];
-  gtag: (...args: any[]) => void;
-  doNotTrack: string | null | undefined;
+declare module 'https://www.gstatic.com/firebasejs/*';
+declare module 'firebase/app' {
+  export interface FirebaseOptions {
+    apiKey?: string;
+    authDomain?: string;
+    projectId?: string;
+    storageBucket?: string;
+    messagingSenderId?: string;
+    appId?: string;
+    measurementId?: string;
+  }
 }
